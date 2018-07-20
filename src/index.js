aba8e8568351c2c382a4071f6391a6340286efdb
W Anders <w@doubleu.codes>
2018-07-20 18:15:36 -0500

    Changed from Mkdocs to React

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import './index.css';
+import Header from 'com/Header';
+
+let components = (
+    <Header />
+);
+
+ReactDOM.render(components, document.getElementById('root'));
