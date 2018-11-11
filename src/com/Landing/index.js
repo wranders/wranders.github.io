2fdc50df2eb94475f65f595e3d22902ffa56e872
W Anders <w@doubleu.codes>
2018-11-11 12:40:38 -0600

    Restructure Made static components stateless

--- src/com/Landing/index.js
+++ src/com/Landing/index.js
@@ -1,28 +1,25 @@
 import React from 'react';
-import Logo from 'com/Landing/Logo'
-import LandingLogo from './Logo/landing_logo.png';
+import Image from 'com/Image'
+import DoubleuLogo from 'img/doubleu_logo.png';
 
-class Landing extends React.Component {
-    render() {
-        return (
-            <div>
-            <Logo 
-                img={LandingLogo}
-                style={{
-                    position: 'fixed',
-                    top: '20%',
-                    left: '50%',
-                    marginRight: '-50%',
-                    transform: 'translate(-50%, -50%)',
-                    width: '20vh',
-                    border: '0.1em solid #ddd',
-                    borderRadius: '25%'
-                }}
-                alt="DoUbleU Logo"
-            />
-            </div>
-        );
-    }
-}
+const Landing = () => {
+    return(
+        <div>
+            <Image 
+            img={DoubleuLogo} 
+            style={{
+                position: 'fixed', 
+                top: '20%', 
+                left: '50%', 
+                marginRight: '-50%', 
+                transform: 'translate(-50%, -50%)', 
+                width: '20vh', 
+                border: '0.1em solid #ddd', 
+                borderRadius: '25%'
+            }} 
+            alt="DoUbleU Logo"/>
+        </div>
+    )
+};
 
 export default Landing;
