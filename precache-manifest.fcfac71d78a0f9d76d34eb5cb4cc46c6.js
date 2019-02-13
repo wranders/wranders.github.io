6d0800299cee8d30fd5b51056761c6955afd249b
W Anders <wranders@protonmail.com>
2019-02-13 00:02:25 -0600

    Updates

---
+++ precache-manifest.fcfac71d78a0f9d76d34eb5cb4cc46c6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "a18b81fad43df8c59fe034d6cac5d50b",
+    "url": "/static/media/nebula.a18b81fa.jpg"
+  },
+  {
+    "revision": "9d34a15cbf08e11eed0325d964548a89",
+    "url": "/static/media/doubleu_logo.9d34a15c.png"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "5e3a376bb31f6b3d1f04",
+    "url": "/static/js/main.5e3a376b.chunk.js"
+  },
+  {
+    "revision": "c34d4d49c28e26dfa496",
+    "url": "/static/js/2.c34d4d49.chunk.js"
+  },
+  {
+    "revision": "5e3a376bb31f6b3d1f04",
+    "url": "/static/css/main.76c9086c.chunk.css"
+  },
+  {
+    "revision": "cc75bd6e3094a7532cc4c495a7daa9e9",
+    "url": "/index.html"
+  }
+];
