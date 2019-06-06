1618e34caee913203ffde0de891132d903b09cde
W Anders <wranders@protonmail.com>
2019-06-06 13:15:29 -0500

    Migration wrap-up

--- src/index.jsx
+++ src/index.jsx
@@ -4,4 +4,14 @@ import './index.css';
 
 import App from 'screens/app';
 
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+        navigator.serviceWorker.register('/service-worker.js').then(registration => {
+            console.log('SW registered', registration);
+        }).catch(registrationError => {
+            console.log('SW registration failed: ', registrationError);
+        });
+    });
+}
+
 render(<App />, document.getElementById('root'))
