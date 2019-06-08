bd61364eeece1308d47b36128598698e9e72ac77
W Anders <wranders@protonmail.com>
2019-06-08 02:56:38 -0500

    Added About placeholder

---
+++ src/screens/about/about.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import './about.css';
+
+const About = () => {
+    return (
+        <div className="about">
+            <div className="content">
+                {/* 
+                    Placeholder
+                */}
+            </div>
+        </div>
+    )
+}
+
+export default About;
