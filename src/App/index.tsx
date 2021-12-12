aad2758aee6540dddcc961f9182484f72f1d8925
W Anders <w@doubleu.codes>
2021-12-11 19:28:06 -0500

    Updated App router for React Router v6

--- src/App/index.tsx
+++ src/App/index.tsx
@@ -6,7 +6,7 @@ import Contact from '@Screens/Contact/Contact';
 import E404 from '@Screens/Error/404';
 import Home from '@Screens/Home/Home';
 import React from 'react';
-import { HashRouter, Route, Switch } from 'react-router-dom';
+import { HashRouter, Route, Routes } from 'react-router-dom';
 import Navigation from './Navigation';
 
 export default function App(): React.ReactElement {
@@ -28,11 +28,11 @@ export default function App(): React.ReactElement {
       <TitleProvider siteName="DoUbleU">
         <HashRouter>
           <Navigation />
-          <Switch>
-            <Route path="/" exact component={Home} />
-            <Route path="/contact" exact component={Contact} />
-            <Route component={E404} />
-          </Switch>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route element={<E404 />} />
+          </Routes>
         </HashRouter>
       </TitleProvider>
     </Theme>
