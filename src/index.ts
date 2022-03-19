d3bb56db1fc89b55845f8ccb9313c57e76ced0a1
W Anders <w@doubleu.codes>
2022-03-19 11:26:42 -0400

    moved serviceworker install to html

--- src/index.ts
+++ src/index.ts
@@ -2,19 +2,6 @@ import React from 'react';
 import ReactRouterDOM from 'react-dom';
 import App from './App';
 
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker
-      .register('/sw.js')
-      .then(() => {
-        console.log('ServiceWorker registered');
-      })
-      .catch(() => {
-        console.log('ServiceWorker registration failed');
-      });
-  });
-}
-
 ReactRouterDOM.render(
   React.createElement(App, null),
   document.getElementById('app-root'),
