0925c3badd9954efbc5a844aaba9fdaad205be01
W Anders <w@doubleu.codes>
2022-06-05 19:05:18 -0400

    renamed 404 error component file

--- src/App/index.tsx
+++ src/App/index.tsx
@@ -3,7 +3,7 @@ import TitleProvider from '@Components/TitleProvider';
 import { ThemeOptions } from '@material-ui/core/styles';
 import { green, red } from '@material-ui/core/colors';
 import Contact from '@Screens/Contact/Contact';
-import E404 from '@Screens/Error/404';
+import E404 from '@Screens/Error/E404';
 import Home from '@Screens/Home/Home';
 import React from 'react';
 import { HashRouter, Route, Routes } from 'react-router-dom';
